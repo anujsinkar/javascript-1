6be198d328e26973bd65c6b683915e53a0ef8471
anujsinkar <162952585+anujsinkar@users.noreply.github.com>
2025-07-16 11:20:23 +0000

    variables and data type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 123441
+let accountEmail = "anujsinkar@gmail.com"
+var accountPassword = "7855489"
+accountCity = "Pune"
+
+
+console.log([accountId, accountEmail, accountPassword, accountCity ]);
+
