5b2152a3a396d282416b8e872a1b6ac1dc2d502f
anujsinkar <162952585+anujsinkar@users.noreply.github.com>
2025-08-17 06:17:28 +0000

    correctionn

--- 04_control_flow/02_switch.js
+++ 04_control_flow/02_switch.js
@@ -1,18 +1,19 @@
-const month = 3
+const month = 12
 
 switch (month) {
     case 1:
         console.log("Jan")
         break;
-switch (month) {
+
     case 2:
         console.log("feb")
         break;
-switch (month) {
+
+ 
     case 3:
         console.log("Mar")
         break;
-switch (month) {
+
     case 4:
         console.log("Apr")
         break;
