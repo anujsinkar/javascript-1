9f7e202138ce846a5fc125bc2ba592bbbaed9b39
anujsinkar <162952585+anujsinkar@users.noreply.github.com>
2025-08-17 06:14:48 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,23 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Jan")
+        break;
+switch (month) {
+    case 2:
+        console.log("feb")
+        break;
+switch (month) {
+    case 3:
+        console.log("Mar")
+        break;
+switch (month) {
+    case 4:
+        console.log("Apr")
+        break;
+
+    default:
+        console.log("Default Statement")
+        break;
+}
